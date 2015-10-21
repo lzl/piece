5b0e4488c30548740e07670faeb750118dcfce5f
lizunlong <lizunlong@gmail.com>
2015-10-21 12:20:39 +0800

    fixed the size of hero title

--- ui/App.jsx
+++ ui/App.jsx
@@ -47,7 +47,7 @@ App = React.createClass({
     if (! this.data.currentUser) {
       return (
         <div className="jumbotron">
-          <h1 className="display-3">Welcome to Piece</h1>
+          <h1 className="display-2">Welcome to Piece.</h1>
           <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
         </div>
       );
