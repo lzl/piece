df1cc918a98400abf1dbd6f96cccdf53268a5ea5
lizunlong <lizunlong@gmail.com>
2015-11-07 09:58:39 +0800

    added DDPRateLimiter and rules

---
+++ core/both/rate_limit.js
@@ -0,0 +1,16 @@
+var methodRule = {
+  type: "method",
+  name: function (name) {
+    return _.contains(['pieceInsert', 'pieceRemove'], name);
+  },
+}
+
+var subscriptionRule = {
+  type: "subscription",
+  name: function (name) {
+    return _.contains(['meteor_autoupdate_clientVersions', 'meteor.loginServiceConfiguration', 'pieceSingleUserPosts', 'pieceCurrentUserPosts', 'pieceAllUserPosts', 'pieceMultiUserPosts'], name);
+  },
+}
+
+DDPRateLimiter.addRule(methodRule);
+DDPRateLimiter.addRule(subscriptionRule);
