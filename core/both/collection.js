93eeb190c9c4d0c3da853466f058bf41df99e433
lizunlong <lizunlong@gmail.com>
2015-12-08 11:35:06 +0800

    deny is enough

--- core/both/collection.js
+++ core/both/collection.js
@@ -1,9 +1,4 @@
 Pieces = new Mongo.Collection('pieces');
-Pieces.allow({
-  insert() { return false; },
-  update() { return false; },
-  remove() { return false; }
-});
 Pieces.deny({
   insert() { return true; },
   update() { return true; },
@@ -11,22 +6,12 @@ Pieces.deny({
 });
 
 Clones = new Mongo.Collection('clones');
-Clones.allow({
-  insert() { return false; },
-  update() { return false; },
-  remove() { return false; }
-});
 Clones.deny({
   insert() { return true; },
   update() { return true; },
   remove() { return true; }
 });
 
-Meteor.users.allow({
-  insert() { return false; },
-  update() { return false; },
-  remove() { return false; }
-});
 Meteor.users.deny({
   insert() { return true; },
   update() { return true; },
