f1628b44e5177c9999c0c990bebbff642b5b34d9
lizunlong <lizunlong@gmail.com>
2015-12-17 14:01:56 +0800

    use 'defaultValue' on <select> instead of setting 'selected' on <option>

--- ui/App.jsx
+++ ui/App.jsx
@@ -8,7 +8,7 @@ App = React.createClass({
       currentUser: Meteor.user(),
       clones: Clones.find({}, {sort: {createdAt: 1}}).fetch(),
       clonesIsReady: handleClones.ready(),
-      currentClone: Clones.findOne({_id: Session.get("currentCloneId")}),
+      currentClone: Clones.find({_id: Session.get("currentCloneId")}).fetch()[0],
       otherClones: Clones.find({_id: {$ne: Session.get("currentCloneId")}}).fetch()
     }
   },
@@ -28,8 +28,8 @@ App = React.createClass({
   renderSelectClone() {
     if (this.data.clones.length > 1) {
       return (
-        <select className="c-select" ref="selectClone" onChange={this.selectClone}>
-          <option value={this.data.currentClone._id} selected>{this.data.currentClone.name}</option>
+        <select className="c-select" ref="selectClone" defaultValue={this.data.currentClone._id} onChange={this.selectClone}>
+          <option value={this.data.currentClone._id}>{this.data.currentClone.name}</option>
           {this.renderOtherClones()}
         </select>
       );
