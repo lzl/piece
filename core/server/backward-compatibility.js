9569ccf3a96b03a5472cf4c478eaf58eaf1038d5
lizunlong <lizunlong@gmail.com>
2015-12-17 22:10:02 +0800

    clean before deploy

--- core/server/backward-compatibility.js
+++ core/server/backward-compatibility.js
@@ -7,8 +7,8 @@ if (hasNotHostname) {
   Pieces.update({hostname: {$exists: false}}, {$set: {hostname: hostname}}, {multi: true});
 }
 // take risk for this one, not all of shared pieces are from our host
-const hasOriginButHasNotHostName = Pieces.find({origin: {$exists: true}, 'origin.hostname': {$exists: false}}).count() !== 0;
-if (hasOriginButHasNotHostName) {
-  console.log("set hostname to the pieces' origin has not");
-  Pieces.update({origin: {$exists: true}, 'origin.hostname': {$exists: false}}, {$set: {'origin.hostname': hostname}}, {multi: true});
-}
+// const hasOriginButHasNotHostName = Pieces.find({origin: {$exists: true}, 'origin.hostname': {$exists: false}}).count() !== 0;
+// if (hasOriginButHasNotHostName) {
+//   console.log("set hostname to the pieces' origin has not");
+//   Pieces.update({origin: {$exists: true}, 'origin.hostname': {$exists: false}}, {$set: {'origin.hostname': hostname}}, {multi: true});
+// }
