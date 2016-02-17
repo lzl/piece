a8df45653371a2fc74bdd962fa1ba76c59f6cb17
lizunlong <lizunlong@gmail.com>
2016-02-17 15:29:24 +0800

    add About component

---
+++ client/components/About/about.jsx
@@ -0,0 +1,6 @@
+About = () =>
+  <div id="about" className={P.isActiveCard('about')}>
+    <p className="card-text">Each piece is one idea. Your idea. People could follow you. Know more about you, piece by piece. And do you know, your best reader may be yourself. Look back your own pieces. Feel and learn from the younger version of yourself.</p>
+    <p className="card-text">Each piece is one idea. Their ideas. You can follow them. Know more about the world, piece by piece. Then you will know, the best way to consume is to build. Build something people love. Build something that just works. Build something you are proud of. Now start with an idea, a piece. Day after day you would know what is it. More important, why and how to build it.</p>
+  </div>
+About.displayName = "About";
