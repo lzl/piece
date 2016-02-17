abc37641e4f915a1283383de37f21d8a3aaabb07
lizunlong <lizunlong@gmail.com>
2016-02-17 13:43:36 +0800

    clean App layout

--- client/layout/app.jsx
+++ client/layout/app.jsx
@@ -5,24 +5,14 @@ App = React.createClass({
       status: Meteor.status().status,
       loggingIn: Meteor.loggingIn(),
       isUser: !!Meteor.user(),
-      isPublic(route) {
-        return [
-          'cloneProfile',
-          'notFound'
-        ].indexOf(route) > -1;
-      },
-      canView() {
-        // return this.isPublic(FlowRouter.getRouteName()) || !!Meteor.user();
-        return !!Meteor.user();
-      }
     };
   },
   render () {
     return (
       <div className="container">
-        <Navigation status={this.data.status} isUser={this.data.isUser}/>
-        {this.data.canView() ? this.props.yield : <Demo loggingIn={this.data.loggingIn} />}
+        <Navigation status={this.data.status} isUser={this.data.isUser} />
+        {this.data.isUser ? this.props.yield : <Demo loggingIn={this.data.loggingIn} />}
       </div>
     );
   }
-})
+});
