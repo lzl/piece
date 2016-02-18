9231706ee633b230402944323d35100081f6ddc3
lizunlong <lizunlong@gmail.com>
2016-02-19 01:06:53 +0800

    set up email templates

--- server/accounts.js
+++ server/accounts.js
@@ -21,3 +21,33 @@ Accounts.onLogin(() => {
     })
   }
 });
+
+Meteor.startup(function () {
+  // set up stmp
+  if (Meteor.settings.private && Meteor.settings.private.smtp) {
+    process.env.MAIL_URL = Meteor.settings.private.smtp;
+  }
+  // override email templates
+  const greet = (welcomeMsg) => {
+    return (user, url) => {
+      const greeting = (user && user.username) ? ("Hello " + user.username + ",") : "Hello,";
+      return `${greeting}
+
+${welcomeMsg}, simply click the link below.
+
+${url}
+
+Thanks.
+`;
+    };
+  };
+  if (Meteor.settings.private && Meteor.settings.private.username && Meteor.settings.private.email) {
+    Accounts.emailTemplates.from = `${Meteor.settings.private.username} <${Meteor.settings.private.email}>`;
+  }
+  if (Meteor.settings.private && Meteor.settings.private.siteName) {
+    Accounts.emailTemplates.siteName = "Piece";
+  }
+  Accounts.emailTemplates.verifyEmail.text = greet("To verify your account email");
+  Accounts.emailTemplates.resetPassword.text = greet("To reset your password");
+  Accounts.emailTemplates.enrollAccount.text = greet("To start using the service");
+});
--- server/email.js
+++
@@ -1,5 +0,0 @@
-if (Meteor.settings.private) {
-  Meteor.startup(function() {
-    process.env.MAIL_URL = Meteor.settings.private.mailgun;
-  });
-}
