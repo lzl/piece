b00740967f2d824d2159b17834c921e3561215c4
lizunlong <lizunlong@gmail.com>
2016-03-16 10:04:19 +0800

    export pieces

--- server/methods.js
+++ server/methods.js
@@ -112,5 +112,21 @@ Meteor.methods({
   sendVerificationEmail() {
     const userId = Meteor.userId();
     return Accounts.sendVerificationEmail(userId);
+  },
+  exportPieces(cloneId) {
+    check(cloneId, String);
+
+    const userId = Meteor.userId();
+    if (! userId) {
+      throw new Meteor.Error("not-authorized", "Log in before export pieces.");
+    }
+
+    const ownedClone = Clones.findOne({_id: cloneId, ownerId: userId});
+    if (ownedClone) {
+      const pieces = Pieces.find({userId: cloneId}, {sort: {createdAt: -1}}).fetch();
+      return JSON.stringify(pieces, null, 2);
+    } else {
+      throw new Meteor.Error("not-authorized", "You don't own that clone.");
+    }
   }
 })
