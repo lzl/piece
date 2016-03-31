0b5189a765a64402187828bda3323279544e671e
lizunlong <lizunlong@gmail.com>
2016-03-31 22:22:53 +0800

    self is better than you

--- client/layout/demo.jsx
+++ client/layout/demo.jsx
@@ -17,7 +17,7 @@ Demo = React.createClass({
             <div className="hr" />
             <div className="hr" />
             <p>I believed in the Internet and I see hyperlink as a core value of it. What this app missed, you will find the counterpart via hyperlink. What we really need is, to recall the words we didn't value and to get the link we didn't know. That's the vision of Piece.</p>
-            <p><mark>Connect with the younger version of you.</mark></p>
+            <p><mark>Connect with the younger version of self.</mark></p>
             <p><mark>Connect with the world of unknown.</mark></p>
             <p>Do you agree? If yes, it's probably a good time to sign up.</p>
           </div>
