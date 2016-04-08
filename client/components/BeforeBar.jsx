efbbfb70ae40a085e44b30290e95711b4ba84c79
lizunlong <lizunlong@gmail.com>
2016-04-08 20:06:10 +0800

    refined the BeforeBar

--- client/components/BeforeBar.jsx
+++ client/components/BeforeBar.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 class BeforeBarComponent extends Component {
   render() {
     const before = Number(this.props.before);
     return (
       <ul className="list-group">
-        <li className="list-group-item">Before {P.publishedAt(Number(before))}</li>
+        <li className="list-group-item">
+          <span>View from {moment(before).format('LL')}</span>
+          <a href="/" className="pull-xs-right">Go to now</a>
+        </li>
       </ul>
     );
   }
